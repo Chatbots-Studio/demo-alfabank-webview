7d50daddcede5a9348995cc25bb2c4d505abb517
yolka85 <4yolka@gmail.com>
2020-09-07 17:16:01 +0300

    Added links to buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,27 @@
-$('.tel-mask').inputmask({
-    mask: "+99 999 999 99 99",
-    placeholder: '',
-    showMaskOnHover: false
-});
-$('.required-field').each(function () {
-    var input = $(this);
-    input.on('input', function () {
-        if (input.val().length > 0) {
-            input.addClass('populated');
-        } else {
-            input.removeClass('populated');
-        };
-        if ($('.required-field').length === $('.required-field.populated').length) {
-            $('#connect-btn').removeClass('disabled');
-        } else {
-            $('#connect-btn').addClass('disabled');
-        }
+if ($('#form').length) {
+    $('.tel-mask').inputmask({
+        mask: "+99 999 999 99 99",
+        placeholder: '',
+        showMaskOnHover: false
     });
-})
+    $('.required-field').each(function () {
+        var input = $(this);
+        input.on('input', function () {
+            if (input.val().length > 0) {
+                input.addClass('populated');
+            } else {
+                input.removeClass('populated');
+            };
+            if ($('.required-field').length === $('.required-field.populated').length) {
+                $('#connect-btn').removeClass('disabled');
+            } else {
+                $('#connect-btn').addClass('disabled');
+            }
+        });
+    });
+    $('#connect-btn').click(function () {
+        if (!($('#connect-btn').hasClass('disabled'))) {
+            $(location).attr('href', 'https://m.me/114032010428817?pro');
+        }
+    })
+}
